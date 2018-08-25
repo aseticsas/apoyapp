1e35a0779e626e8d9b59c464d706a7e8a0bec645
Jessica Sandoval <soporte@asetic.co>
2018-08-25 08:17:24 -0500

    page_inicio

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InicioPage } from '../inicio/inicio';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController) {  }
+    goToOtherPage()
+    {
+    this.navCtrl.push(InicioPage);
+    }
 }
